8bd653930a737bc003439476feccfc4138989f65
Witler <85369599+bigkid-dev@users.noreply.github.com>
2022-06-24 14:18:54 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let fullName = 'Egbeyemi Ireoluwa';
+console.log('my name is ',fullName);
+
+let height = 6.1;
+console.log('my height is ', height);
+
+let country = 'Nigeria';
+console.log('My country is ', country);
